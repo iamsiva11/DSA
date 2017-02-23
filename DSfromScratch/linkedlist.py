2f729cb76ab0655c2fcbf9afaad71837c302a9dd
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-02-23 23:34:41 +0530

    first commit - linked list

---
+++ DSfromScratch/linkedlist.py
@@ -0,0 +1,65 @@
+class Node:
+
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+                
+class LinkedList:
+    
+        def __init__(self):
+            self.head = None
+        
+        def add(self, data):
+            node = Node(data)
+            if  self.head==None:
+                self.head=node
+            else:
+                node.next=self.head
+                self.head=node
+          
+        def __str__(self):
+            s = ""
+            p = self.head
+            while(p.next != None):
+                s+=p.data
+                p=p.next
+            s+= p.data
+            return s
+        
+                        
+#         def remove(self, p):
+#             old_curr = self.head 
+#             curr = self.head 
+#             while(curr.next != None): 
+#                 if (curr.data == p.data):
+#                     #p.data = p.next.data
+#                     #p.next=p.next.next
+#                     curr.data = curr.next.data
+#                     curr.next = curr.next.next
+#             return old_curr   
+
+        def delete(self, data):
+            current = self.head
+            prev = None
+            while current:
+                if current.data == data:
+                    if current == self.head:
+                        self.head = current.next
+                    else:
+                        prev.next = current.next
+                    return current
+                prev = current
+                current = current.next
+            return None
+            
+
+if __name__ == '__main__':
+    l = LinkedList()
+    l.add( 'a' )
+    l.add( 'b' )
+    d  =  l.add( 'c' )
+    print l      
+    l.delete('a')
+    print l 
+
+
