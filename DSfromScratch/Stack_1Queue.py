2ed97fc61f5f8c827954af40cd32ea84357ac37e
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-03-02 01:09:35 +0530

    stack - with single queue

---
+++ DSfromScratch/Stack_1Queue.py
@@ -0,0 +1,38 @@
+class Stack:
+
+	def __init__(self):
+		self.q=[]
+
+	def push(self,x):
+		self.q.append(x)
+
+	def pop(self):
+
+		n=len(self.q)
+		pop_element=None
+
+		for _ in range(n):
+			self.q.append(self.q[0])
+			del self.q[0]
+
+		return self.q.pop()
+    
+	def __str__(self):
+		return ",".join(map(str,self.q))
+		#return self.q
+    
+
+
+if __name__=="__main__":
+	stck=Stack()
+	stck.push(2)
+	stck.pop()
+	stck.push(3)
+	stck.push(4)
+	stck.push(6)
+	stck.pop()
+	print stck
+
+"""
+3,4
+"""	
