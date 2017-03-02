1a1aef8545f6468aed9d5eabc4351c171e465611
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-03-03 00:34:11 +0530

    doc string , updated code

--- DSfromScratch/Stack_2Queues.py
+++ DSfromScratch/Stack_2Queues.py
@@ -1,26 +1,56 @@
+"""
+Every time when pushing one element into stack, push queue 1's top in queue 2, and push the new element in queue 1.
+
+Every time when popping the top element in stack, push the queue 1's top (only one element in this queue). 
+Then push n-1 elements from queue 2 to queue 1, where n is the total number of elements in queue 2.
+In other words, we left one element in queue 2 (this is the top element in the current stack), and push all the other elements into the empty queue (queue 1).
+Finally we swap queue 1 and queue 2. 
+The stack is empty iff queue 1 and queue 2 are all empty. The top element is always the only element in queue 1.
+
+"""
+
 class Stack:
 	def __init__(self):
 		self.q1 = []
 		self.q2 = []
 
 	def push(self, x):
-		self.q1.append(x)
-		if len(self.q1==1):
-			return
-		else:
-			self.q2.append(self.q1.pop(0))
+        """
+        Push element x onto stack.
+        :type x: int
+        :rtype: void
+        """
+        self.q1.append(x)
+        if(len(self.q1)==1): #if it was empty
+            return
+        else:    
+            self.q2.append(self.q1.pop(0)) #or just pop() , since q1 len is always 1
+        
 
-	def pop(self):
-		self.q1.pop(0)
-		for i in range(len(self.q2)-1):
-			self.q1.append(self.q2.pop(0))
-	    self.q1, self.q2 =  self.q2, self.q1
+    def pop(self):
+        """
+        Removes the element on top of the stack and returns that element.
+        :rtype: int
+        """
+        if self.q1:
+            for i in xrange(len(self.q2)-1):
+                self.q1.append(self.q2.pop(0))
+            self.q1, self.q2 = self.q2, self.q1 
+            return self.q1.pop() if self.q1 else None #or pop(0)
+        
+        
 
-	def top(self):
-		return self.q1[0]
+    def top(self):
+        """
+        Get the top element.
+        :rtype: int
+        """
+        return self.q1[0] if self.q1 else None
+        
 
-	def empty(self):
-		if len(self.q1)==0 and len(self.q2)==0:
-			return True
-		else:
-			return False
+    def empty(self):
+        """
+        Returns whether the stack is empty.
+        :rtype: bool
+        """
+        return len(self.q2)==0 and len(self.q1) ==0
