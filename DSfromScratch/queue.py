493deab6d45b6a5439800b6eea6d7349f05c0f4a
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-03-06 00:00:56 +0530

    docstring , improved code

--- DSfromScratch/queue.py
+++ DSfromScratch/queue.py
@@ -1,28 +1,47 @@
-#queue
-class queue:
+"""FIFO queue implementation using a Python list as underlying storage."""
+class ArrayQueue:
     
     def __init__(self):
-        self.items=[]        
+        """Create an empty queue."""
+        self._items=[]        
 
     def enqueue(self, item):
-        self.items.append(item)
+        """Add an element to the back of queue."""
+        self._items.append(item)
     
     def dequeue(self):
+        """Remove and return the first element of the queue (i.e., FIFO).
+        Raise Empty exception if the queue is empty."""
+
         if (not self.isEmpty()):  
-            self.items = self.items[1:]            
+            temp = self._items[0]
+            self._items = self._items[1:]            
+            del temp
 
     def isEmpty(self):
-        return len(self.items)==0
-        #return self.items == []
+        return len(self._items)==0
+
+    def first(self):
+        """Return (but do not remove) the element at the front of the queue.
+        Raise Empty exception if the queue is empty."""
+        if self.isEmpty():
+            print "Queue is empty"
+        return self._items[0]
     
     def __str__(self):
-        return " ".join(str(self.items))
+        """ Print the queue object as a string"""
+        return " ".join(str(self._items))
     
+    def __len__(self):
+    """Return the number of elements in the queue."""
+        return len(self._items)
+
 if __name__ == "__main__":
-    q = queue()
+    q = ArrayQueue()
     q.enqueue(2)
     q.enqueue(3)
-    q.enqueue(4)
     q.dequeue()
+    q.enqueue(4)    
+    q.enqueue(5)
     q.dequeue()
     print q
