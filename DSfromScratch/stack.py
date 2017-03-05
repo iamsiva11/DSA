8b08a91e417de3119d79996d744c6ea74b96b268
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-03-05 23:50:50 +0530

    added doc, imporved code

--- DSfromScratch/stack.py
+++ DSfromScratch/stack.py
@@ -1,35 +1,47 @@
-class stack:
+class ArrayStack:
 
     def __init__(self):
-        self.items=[]
-    
-    def __str__(self):
-        return " ".join(str(self.items))
+        """Create an empty stack."""
+        self._items=[] # nonpublic list instance
     
     def __len__(self):
-        return len(self.items)
+        """Return the number of elements in the stack."""
+        return len(self._items)
     
     def push(self,item):
-        self.items.append(item)
+        """Add element to the top of the stack."""
+        self._items.append(item)
         
     def pop(self):
-        #return self.items[len(self.items)-1]
-        return self.items.pop()
+        """Remove and return the element from the top of the stack (i.e., LIFO).
+        Raise Empty exception if the stack is empty."""
+        if self.isEmpty():
+            print "Stack is empty'"
+        return self._items.pop()
     
     def isEmpty(self):
-        return (len(self.items)==0)
+        """Return True if the stack is empty."""
+        return (len(self._items)==0)
 
-    def peek(self):
-        return self.items[-1]
+    def top(self):
+        """Return (but do not remove) the element at the top of the stack.
+        Raise Empty exception if the stack is empty.
+        """
+        if self.isEmpty():
+            #raise Empty('Stack is empty')
+            print "Stack is empty'"
+        return self._items[-1]
+
+    def __str__(self):
+        """ Print the stack object as a string"""
+        return " ".join(str(self._items))
     
-        
 if __name__ == "__main__":
-    s = stack()
+    s = ArrayStack()
     s.push(1)
     print s
     s.push("s")
     s.push("e")
     s.push("d")
     s.pop()
-    print s
-    
+    print s    
